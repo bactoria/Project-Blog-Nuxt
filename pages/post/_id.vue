3fc0f7a7ebca44bbd2139ffd154a994d720a6b11
bactoria <bactoria@gmail.com>
2018-05-29 14:24:40 +0900

    Improve SEO cuz my posts has wrong title in google search engine

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -6,24 +6,24 @@
 
       <!-- 카테고리 -->
       <div align="center">
-        <a class = "GodoB" style= "font-size: 1.3em; text-decoration: none; color: #999999;" :href="'/category/'+post.category.id">{{post.category.name}}</a>
+        <a class="GodoB" style="font-size: 1.3em; text-decoration: none; color: #999999;"
+           :href="'/category/'+post.category.id">{{post.category.name}}</a>
       </div>
 
 
       <!--<a href="#disqus_thread"></a>-->
 
       <div align="center">
-      <!-- 제목 -->
-        <span class="GodoB PostTitle">{{post.title}}</span>
+        <!-- 제목 -->
+        <div class="GodoB PostTitle">{{post.title}}</div>
 
 
-        <div style="border-bottom: 0.6em solid #dddddd;border-radius: 2px; width: 10%; margin-top: 3em"> </div>
+        <div style="border-bottom: 0.6em solid #dddddd;border-radius: 2px; width: 10%; margin-top: 2em"></div>
 
-        <div class="PostDate"> {{post.createdDate.split("T")[0]}} </div>
+        <span class="PostDate"> {{post.createdDate.split("T")[0]}} </span>
 
         <!-- 날짜 -->
       </div>
-
 
 
     </div>
@@ -31,30 +31,31 @@
     <article>
       <!-- 내용 -->
 
-      <vue-markdown style="text-align: left;" class="GodoM markdown" :watches="['show','html','breaks','linkify','emoji','typographer','toc']"
+      <vue-markdown style="text-align: left;" class="GodoM markdown"
+                    :watches="['show','html','breaks','linkify','emoji','typographer','toc']"
                     :source="post.content" :show="show" :html="html" :breaks="breaks" :linkify="linkify"
                     :emoji="emoji" :typographer="typographer"></vue-markdown>
 
       <div align="center">
-      <a href="#" style="text-decoration: none"><v-icon style="right:0;
-       font-size: 60px;">arrow_drop_up</v-icon></a>
+        <a href="#" style="text-decoration: none">
+          <v-icon style="right:0;
+       font-size: 60px;">arrow_drop_up
+          </v-icon>
+        </a>
       </div>
     </article>
     <!-- Facebook 좋아요 --> <!--Docs- https://developers.facebook.com/docs/plugins/like-button -->
-      <div class="fb-like"
+    <div class="fb-like"
          :data-href="'https://bactoria.me/post/'+post.id"
          data-layout="standard"
          data-action="like"
          data-show-faces="true"
          data-share="true">
-      </div>
-
-
-
+    </div>
 
 
     <!-- Facebook 댓글 --> <!--Docs- https://developers.facebook.com/docs/plugins/comments/-->
-      <a href="#disqus_thread"></a>
+    <a href="#disqus_thread"></a>
     <!-- <div id="fb-root"></div>
          <div class="fb-comments" data-href="http://localhost:3000" data-width="100%" data-numposts="5"></div>
     -->
@@ -63,48 +64,54 @@
     <div id="disqus_thread"></div>
     <script>
 
-    /**
-     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-    /*
-    var disqus_config = function () {
-    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    */
-    (function() { // DON'T EDIT BELOW THIS LINE
-      var d = document, s = d.createElement('script');
-      s.src = 'https://bactoria.disqus.com/embed.js';
-      s.setAttribute('data-timestamp', +new Date());
-      (d.head || d.body).appendChild(s);
-    })();
-  </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+      /**
+       *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+       *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+      /*
+      var disqus_config = function () {
+      this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+      this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+      };
+      */
+      (function () { // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        s.src = 'https://bactoria.disqus.com/embed.js';
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+      })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by
+      Disqus.</a></noscript>
 
   </div>
 </template>
 
 
-
-  <script>
+<script>
   import MarkdownIt from 'markdown-it'
   import VueMarkdown from 'vue-markdown'
 
   export default {
-    head: {
-      script: [
-        {src: 'https://bactoria.disqus.com/count.js', id: 'dsq-count-scr', body: true},
-        {src: '/js/main.js'},
-        {src: '/js/prism.js'},
-        {src: '/js/facebookSDK.js'}
-      ],
-      link: [
-        { rel: 'stylesheet', href: '/css/main.css' },
-        { rel: 'stylesheet', href: '/css/prism.css' },
-        { rel: 'stylesheet', href: '/css/responsive.scss', lang:'scss' }
-      ]
+    head() {
+      return {
+
+        title: this.post.title + ' :: Bactoria',
+        meta: [
+          { hid: 'description', name: 'description', content: this.post.content },
+        ],
+        script: [
+          {src: 'https://bactoria.disqus.com/count.js', id: 'dsq-count-scr', body: true},
+          {src: '/js/main.js'},
+          {src: '/js/prism.js'},
+          {src: '/js/facebookSDK.js'}
+        ],
+        link: [
+          {rel: 'stylesheet', href: '/css/main.css'},
+          {rel: 'stylesheet', href: '/css/prism.css'}
+        ]
+      }
     },
-    data () {
+    data() {
       return {
         //        result2: new MarkdownIt().render(this.$store.state.post.content),
         post: this.$store.state.post,
@@ -122,13 +129,13 @@
     middleware: 'post',
     components: {VueMarkdown}
   }
-  </script>
+</script>
 
 <style lang="scss" scoped>
 
   .root {
-    margin-left : 5%;
-    margin-right : 5%;
+    margin-left: 5%;
+    margin-right: 5%;
     max-width: 100%;
   }
 
@@ -142,7 +149,6 @@
     font-size: 1.2em;
     font-family: GodoM;
   }
-
 
   .markdown {
     max-width: 100%;
@@ -181,7 +187,6 @@
     'md': (max-width: 1200px)
   ) !default;
 
-
   /* responsive scss */
 
   @mixin respond-to($breakpoint) {
@@ -211,8 +216,6 @@
       font-size: 0.7em;
     }
   }
-
-
 
 
 </style>
