a73be9cd89e3ed9ab16f5139c5a1e86c0526e19d
bactoria <bactoria@gmail.com>
2018-06-08 00:00:05 +0900

    Test for SPA

---
+++ components/PostList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="root" align="center">
+    <!-- post
+    [0] : id
+    [1] : categoryId
+    [2] : title
+    [3] : createdDate
+    -->
+    <section style="height: 20px;"></section>
+    <v-card class="card" v-for="post in posts" :key="post[0]">
+      <div @click="getPost(post[0])">asd</div>
+      <a class="none_" :href="'/post/'+post[0]">
+        <div class="GodoB title">
+          {{post[2]}}
+        </div>
+        <div align="right" class="GodoM" style="padding-right: 10px;">
+          {{post[3].split("T")[0]}}
+        </div>
+      </a>
+    </v-card>
+
+  </div>
+
+</template>
+
+<script>
+  export default {
+    computed: {
+      posts() {
+        return this.$store.state.posts.content
+      },
+    },
+    methods: {
+      getPost(id) {
+        this.$router.push('/post/' + id);
+      }
+    }
+
+  }
+</script>
+
+<style lang="scss" scoped>
+
+  .root {
+    margin-left: 7%;
+    margin-right: 7%;
+  }
+
+  .card {
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 6px, rgba(0, 0, 0, 0.3) 0px 1px 4px;
+    margin: 15px;
+
+    padding-top: 20px;
+    padding-bottom: 20px;
+
+    transition: .15s all ease-in-out;
+    background-image: url("/static/image/backboard.jpg");
+    background-repeat: repeat;
+
+    &:hover {
+      transform: scale(1.07);
+    }
+
+    a {
+      text-decoration: none;
+      cursor: pointer;
+      color: black;
+
+      .title {
+        padding-top: 1.5rem;
+      }
+    }
+  }
+
+</style>
--- pages/category/_id.vue
+++ pages/category/_id.vue
@@ -1,82 +1,18 @@
 <template>
-<div class="root" align="center">
-<!-- post
-[0] : id
-[1] : categoryId
-[2] : title
-[3] : createdDate
--->
-  <section style="height: 20px;"> </section>
-  <v-card class="card" v-for="post in posts" :key="post[0]">
-    <a class="none_" :href="'/post/'+post[0]">
-      <div class="GodoB title">
-        {{post[2]}}
-      </div >
-      <div align="right" class= "GodoM" style="padding-right: 10px;">
-        {{post[3].split("T")[0]}}
-       </div>
-     </a>
-  </v-card>
+  <post-list>
 
-</div>
+  </post-list>
 </template>
 
-
-
-
 <script>
-  import VueMarkdown from 'vue-markdown'
+  import PostList from "../../components/PostList";
 
   export default {
     middleware: 'posts',
-    components: {VueMarkdown},
-    computed: {
-      posts () { return this.$store.state.posts.content},
-    },
-    methods: {
-      getPost (id) {
-        this.$router.push('/post/'+id);
-      }
-    }
+    components: {PostList},
   }
 </script>
 
 <style lang="scss" scoped>
-
-  .root {
-    margin-left:7%;
-    margin-right:7%;
-  }
-
-  .card {
-    box-shadow: rgba(0, 0, 0, 0.3) 0px 1px 6px, rgba(0, 0, 0, 0.3) 0px 1px 4px;
-    margin: 15px;
-
-    padding-top: 20px;
-    padding-bottom: 20px;
-
-    transition: .15s all ease-in-out;
-    background-image: url("/static/image/backboard.jpg");
-    background-repeat: repeat;
-
-    &:hover {
-      transform: scale(1.07);
-    }
-
-
-    a {
-      text-decoration: none;
-      cursor:pointer;
-      color: black;
-
-
-      .title {
-        padding-top: 1.5rem;
-      }
-    }
-  }
-
-
-
 
 </style>
