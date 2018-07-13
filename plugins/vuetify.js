8dea4a409401314b7c2f8c337a7a852737ea585c
bactoria <bactoria@gmail.com>
2018-07-13 23:42:55 +0900

    change primary color to baemin's main color

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,7 +4,7 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#b24ff3', // a color that is not in the material colors palette
+    primary: '#2ac1bc', // a color that is not in the material colors palette
     accent: colors.grey.darken3,
     secondary: colors.amber.darken3,
     info: colors.teal.lighten1,
