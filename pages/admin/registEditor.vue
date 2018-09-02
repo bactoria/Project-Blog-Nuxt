3298a0e31120b18da435ea33fa9b5d18df6ced5a
bactoria <bactoria@gmail.com>
2018-09-02 16:37:18 +0900

    Make Module Base64, Can see category's name

--- pages/admin/registEditor.vue
+++ pages/admin/registEditor.vue
@@ -23,125 +23,34 @@
       </v-btn>
     </v-dialog>
 
-<!--    <div>{{content}}</div>-->
+
 
     <v-form ref="form">
+
       <!--Title-->
       <v-text-field v-model="title" :counter="20" label="Title" required></v-text-field>
 
-      <!--<v-text-field v-model="content" full-width="true" multi-line="true"></v-text-field>
--->
-      <!--Content-->
-
-      <div id="editSection"></div>
-
-
       <!--Category-->
-      <v-select v-model="category" :items="categories" required>
+      <v-select label="Category" v-model="category" :items="categories" item-text="name" required></v-select>
 
-
-      </v-select>
+      <!--Content-->
+      <div id="editSection"></div>
 
     </v-form>
 
-  <!--  <tui-editor/>-->
+    <!--<tui-editor/>-->
 
     <v-btn @click="addToAPI">등록</v-btn>
 
   </div>
 </template>
 
+
 <script>
   import {mapGetters} from 'vuex'
   import axios from '~/plugins/axios'
   import TuiEditor from '../../components/TuiEditor'
-
-  var Base64 = {
-    _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=", encode: function (e) {
-      var t = "";
-      var n, r, i, s, o, u, a;
-      var f = 0;
-      e = Base64._utf8_encode(e);
-      while (f < e.length) {
-        n = e.charCodeAt(f++);
-        r = e.charCodeAt(f++);
-        i = e.charCodeAt(f++);
-        s = n >> 2;
-        o = (n & 3) << 4 | r >> 4;
-        u = (r & 15) << 2 | i >> 6;
-        a = i & 63;
-        if (isNaN(r)) {
-          u = a = 64
-        } else if (isNaN(i)) {
-          a = 64
-        }
-        t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
-      }
-      return t
-    }, decode: function (e) {
-      var t = "";
-      var n, r, i;
-      var s, o, u, a;
-      var f = 0;
-      e = e.replace(/[^A-Za-z0-9+/=]/g, "");
-      while (f < e.length) {
-        s = this._keyStr.indexOf(e.charAt(f++));
-        o = this._keyStr.indexOf(e.charAt(f++));
-        u = this._keyStr.indexOf(e.charAt(f++));
-        a = this._keyStr.indexOf(e.charAt(f++));
-        n = s << 2 | o >> 4;
-        r = (o & 15) << 4 | u >> 2;
-        i = (u & 3) << 6 | a;
-        t = t + String.fromCharCode(n);
-        if (u != 64) {
-          t = t + String.fromCharCode(r)
-        }
-        if (a != 64) {
-          t = t + String.fromCharCode(i)
-        }
-      }
-      t = Base64._utf8_decode(t);
-      return t
-    }, _utf8_encode: function (e) {
-      e = e.replace(/rn/g, "n");
-      var t = "";
-      for (var n = 0; n < e.length; n++) {
-        var r = e.charCodeAt(n);
-        if (r < 128) {
-          t += String.fromCharCode(r)
-        } else if (r > 127 && r < 2048) {
-          t += String.fromCharCode(r >> 6 | 192);
-          t += String.fromCharCode(r & 63 | 128)
-        } else {
-          t += String.fromCharCode(r >> 12 | 224);
-          t += String.fromCharCode(r >> 6 & 63 | 128);
-          t += String.fromCharCode(r & 63 | 128)
-        }
-      }
-      return t
-    }, _utf8_decode: function (e) {
-      var t = "";
-      var n = 0;
-      var r = c1 = c2 = 0;
-      while (n < e.length) {
-        r = e.charCodeAt(n);
-        if (r < 128) {
-          t += String.fromCharCode(r);
-          n++
-        } else if (r > 191 && r < 224) {
-          c2 = e.charCodeAt(n + 1);
-          t += String.fromCharCode((r & 31) << 6 | c2 & 63);
-          n += 2
-        } else {
-          c2 = e.charCodeAt(n + 1);
-          c3 = e.charCodeAt(n + 2);
-          t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
-          n += 3
-        }
-      }
-      return t
-    }
-  }
+  import Base64 from '~/static/js/Base64'
 
   if (process.browser) {
     var Editor = require('tui-editor');
@@ -166,11 +75,13 @@
         dialog: true,
         authUser: '',
         authPassword: ''
-
       }
     },
-    computed:
-      mapGetters(['categories', 'tokenStr']),
+    computed: mapGetters({
+        categories : 'categories',
+        tokenStr : 'tokenStr',
+        markContent: 'markContent'
+    }),
     methods: {
       addTokenStr() {
         this.dialog = false
@@ -188,7 +99,6 @@
         let tokenStr = this.tokenStr;
 
         console.log(tokenStr)
-        console.log(this.$store.state.tokenStr)
 
         axios.post('/posts', post, {headers: {"Authorization": tokenStr}}
         )
